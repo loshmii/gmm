76843d20ff7b826237887d6726848b94c7e391a6
loshmii <tomicmilos03@gmail.com>
2025-06-12 14:01:44 +0200

    Scratch

--- scribble.py
+++
@@ -1,9 +0,0 @@
-import jax
-import jax.numpy as jnp
-from gmm import GaussianMixture
-
-key = jax.random.key(0)
-true_means = jnp.array([[0.0, 0.0], [4.0, 4.0]])
-X = jax.random.normal(key, (300, 2)) + true_means[jax.random.randint(key, (300,), 0, 2)]
-model = GaussianMixture(2, rng_key=key).fit(X)
-model.plot(iter_losses=True, cluster_scatter=True)
